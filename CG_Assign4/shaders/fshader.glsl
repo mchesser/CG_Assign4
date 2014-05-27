a65506053c877995fc8eb684d4d03d408da88c36
quvarxa <michaelchesser@outlook.com>
2014-05-27 23:36:33 +0930

    Small changes to fragment shader.

--- CG_Assign4/shaders/fshader.glsl
+++ CG_Assign4/shaders/fshader.glsl
@@ -20,6 +20,13 @@ struct Material
 };
 uniform Material material;
 
+vec2 poissonDisk[4] = vec2[](
+	vec2(-0.94201624, -0.39906216),
+	vec2(0.94558609, -0.76890725),
+	vec2(-0.094184101, -0.92938870),
+	vec2(0.34495938, 0.29387760)
+);
+
 void main(void)
 {
 	// Compute lighting
@@ -34,23 +41,13 @@ void main(void)
 
 	// Compute visibility
 	float visibility = 1.0;
-	
-	vec2 poissonDisk[4] = vec2[](
-		vec2(-0.94201624, -0.39906216),
-		vec2(0.94558609, -0.76890725),
-		vec2(-0.094184101, -0.92938870),
-		vec2(0.34495938, 0.29387760)
-	);
-	
-	//for (int i=0;i<4;i++ ){
-	//	visibility -= 0.2*(1.0 - shadow2D(shadowMap, vec3(shadowCoord.xy + poissonDisk[i]/3700.0, (shadowCoord.z-bias)/shadowCoord.w)));
-	//}
-
-	visibility = (texture(shadowMap, vec3(shadowCoord.xy, (shadowCoord.z-bias)/shadowCoord.w)));
+	for (int i = 0; i < 4; i++) {
+		visibility -= 0.2 * (1.0 - texture(shadowMap, vec3(shadowCoord.xy + poissonDisk[i]/3700.0, (shadowCoord.z-bias)/shadowCoord.w)));
+	}
 
     vec4 color = 0.5 * ambient + visibility * diffuse;
     color.a = 1.0;
 
-    vec4 texcolor = texture2D(modelTexture, texcoord);
+    vec4 texcolor = texture(modelTexture, texcoord);
     out_color = texcolor * color;
 }
