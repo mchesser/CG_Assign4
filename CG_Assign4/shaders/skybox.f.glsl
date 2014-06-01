5aa51735b32cb381cd0cd571b81a87f80229b966
Tullie <tulliemurrell@gmail.com>
2014-06-01 12:59:00 +0930

    Fixing shaders so that they work on mac

--- CG_Assign4/shaders/skybox.f.glsl
+++ CG_Assign4/shaders/skybox.f.glsl
@@ -12,19 +12,19 @@ uniform vec3 sun_position;
 
 void main() {
     if (sun_position.y < 0.0) {
-        fColor = texture2D(night_texture, texCoord);
+        fColor = texture(night_texture, texCoord);
 
     } else if (sun_position.y < 100.0) {
-        vec4 night_color = texture2D(night_texture, texCoord);
-        vec4 sunset_color = texture2D(sunset_texture, texCoord);
+        vec4 night_color = texture(night_texture, texCoord);
+        vec4 sunset_color = texture(sunset_texture, texCoord);
         fColor = mix(night_color, sunset_color, sun_position.y / 100.0);
 
     } else if (sun_position.y < 200.0) {
-        vec4 sunset_color = texture2D(sunset_texture, texCoord);
-        vec4 day_color = texture2D(day_texture, texCoord);
+        vec4 sunset_color = texture(sunset_texture, texCoord);
+        vec4 day_color = texture(day_texture, texCoord);
         fColor = mix(sunset_color, day_color, (sun_position.y - 100) / 100.0);
         
     } else {
-        fColor = texture2D(day_texture, texCoord);
+        fColor = texture(day_texture, texCoord);
     }
 }
