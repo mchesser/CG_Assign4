bad2b661d037781ca62dd296caf03805b872b1a0
quvarxa <michaelchesser@outlook.com>
2014-06-10 21:13:37 +0930

    Fragment shader fixes.

--- CG_Assign4/shaders/fshader.glsl
+++ CG_Assign4/shaders/fshader.glsl
@@ -42,12 +42,12 @@ struct Material {
 };
 uniform Material material;
 
-vec2 poissonDisk[4] = vec2[](
-    vec2(-0.94201624, -0.39906216),
-    vec2(0.94558609, -0.76890725),
-    vec2(-0.094184101, -0.92938870),
+vec2 poissonDisk[4] = vec2[] ( 
+    vec2(-0.94201624, -0.39906216), 
+    vec2(0.94558609, -0.76890725), 
+    vec2(-0.094184101, -0.92938870), 
     vec2(0.34495938, 0.29387760)
-    );
+);
 
 uniform float renderDistance;
 uniform vec4 fogColor;
@@ -110,7 +110,11 @@ void main(void) {
         // Shadows
         float bias = 0.005;
         bias = clamp(bias, 0, 0.01);
-        float visibility = texture(shadowMap, vec3(shadowCoord.xy, (shadowCoord.z - bias) / shadowCoord.w));
+        float visibility = 1.0;
+        for (int i = 0; i < 4; ++i) {
+            visibility -= 0.2 * (1.0 - texture(shadowMap, vec3(shadowCoord.xy + poissonDisk[i]/4000.0,
+                (shadowCoord.z - bias) / shadowCoord.w)));
+         }
 
         color = visibility * diffuse * vec4(sunDiffuse, 1.0);
         color.a = 1.0;
